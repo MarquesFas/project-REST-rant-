9e8c9de9dcd14bfe96c20b316eb148ea9e429374
MarquesFas <102706829+MarquesFas@users.noreply.github.com>
2024-05-27 13:59:30 -0400

    Added the edit functionality

--- Views/places/edit.jsx
+++ Views/places/edit.jsx
@@ -1,11 +1,24 @@
 const React = require('react')
 const Def = require('../default.jsx')
 
-function edit_form () {
+function edit_form (data) {
     return (
         <Def>
           <main>
             <h1>Edit Place</h1>
+            <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+                <div className="form-group">
+                    <label htmlFor="name">Place Name</label>
+                    <input 
+                        className="form-control" 
+                        id="name" 
+                        name="name" 
+                        value={data.place.name} 
+                    required />
+                </div>
+            ...
+            </form>
+
           </main>
         </Def>
     )
